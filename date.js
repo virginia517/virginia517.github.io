7631ab193946f11aa51793cb9b9d1cacdefda556
virginia517 <virginiavirginia718@gmail.com>
2020-09-18 13:49:04 -0700

    update

--- date.js
+++ date.js
@@ -1,10 +1,9 @@
-let days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-let monthList=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-let now=new Date();
-let weekDays=days[now.getDay()];
-let date= now.getDate();
-let month=monthList[now.getMonth()];
-let year=now.getFullYear();
-let fullDate= weekDays + "," +" " + month + " " + date + "," + " " + year;
+var days=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var monthList=["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var now=new Date();
+var weekDays=days[now.getDay()];
+var date= now.getDate();
+var month=monthList[now.getMonth()];
+var year=now.getFullYear();
+var fullDate= weekDays + "," +" " + month + " " + date + "," + " " + year;
 document.getElementById('currentDate').textContent=fullDate;
-
