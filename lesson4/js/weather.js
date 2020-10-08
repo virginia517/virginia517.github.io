c3cd5b2a029610849c11cd68c54a8f115d6b4377
virginia517 <virginiavirginia718@gmail.com>
2020-10-08 12:22:09 -0700

    update

--- lesson4/js/weather.js
+++ lesson4/js/weather.js
@@ -7,8 +7,8 @@ var month=monthList[now.getMonth()];
 var year=now.getFullYear();
 var fullDate= weekDays + "," +" " + month + " " + date + "," + " " + year;
 document.getElementById('currentDate').textContent=fullDate;*/
-var dateNow=new Date(document.lastModified);
-document.getElementById('currentDate').textContent=dateNow;
+/*var dateNow=new Date(document.lastModified);
+document.getElementById('currentDate').textContent=dateNow;*/
 
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
@@ -20,3 +20,32 @@ function toggleMenu() {
   } catch (e) {
     alert('Error with code or your browser does not support Locale');
   }*/
+  const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+document.getElementById("currentDate").textContent = fulldate;
