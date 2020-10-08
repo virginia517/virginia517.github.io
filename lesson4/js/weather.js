02dd75e2c84a7a7fe2b3d953e7fa477734e1bd39
virginia517 <virginiavirginia718@gmail.com>
2020-10-08 11:48:14 -0700

    update

--- lesson4/js/weather.js
+++ lesson4/js/weather.js
@@ -15,5 +15,7 @@ function toggleMenu() {
 }
 
 try {
-  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-  document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+  const options = {
+    weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+  }
