74d7432f9037545ee4d45e30f4f37cfc8c6e6e37
virginia517 <virginiavirginia718@gmail.com>
2020-10-08 11:51:29 -0700

    update

--- lesson4/js/weather.js
+++ lesson4/js/weather.js
@@ -15,7 +15,8 @@ function toggleMenu() {
 }
 
 try {
-  const options = {
-    weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-    document.getElementById('currentDate').textContent = new Date().toLocaleDateString('en-US', options);
+    const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
+    document.getElementById('currentdate2').textContent = new Date().toLocaleDateString('en-US', options);
+  } catch (e) {
+    alert('Error with code or your browser does not support Locale');
   }
