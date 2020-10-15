32a148f85cbc60e01313355edef25840dea09c95
virginia517 <virginiavirginia718@gmail.com>
2020-10-15 12:49:48 -0700

    update

--- lesson5/js/weather5.js
+++ lesson5/js/weather5.js
@@ -1,5 +1,15 @@
-let now= new Date();
-let dayOfWeek= now.getDay();
-if(dayOfWeek==5){
-	document.querySelector(".message").style.display="block";
+/*function displayMessage(){
+	let now= new Date();
+    let dayOfWeek= now.getDay();
+    if(dayOfWeek==5){
+	document.querySelector("message").style.display="block";
+}
+}*/
+
+displayMessage();
+function displayMessage(){
+    let theDate = new Date();
+    if (theDate.getDay() == 4) {
+        document.querySelector(".message").style.display = "block";
+    }
 }
