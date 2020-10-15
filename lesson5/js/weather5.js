4b61b30362a0212d3784808cbbde2d1c4cba22a5
virginia517 <virginiavirginia718@gmail.com>
2020-10-15 13:10:46 -0700

    update

--- lesson5/js/weather5.js
+++ lesson5/js/weather5.js
@@ -1,14 +1,14 @@
-/*function displayMessage(){
+function displayMessage(){
 	let now= new Date();
     let dayOfWeek= now.getDay();
     if(dayOfWeek==5){
 	document.querySelector("message").style.display="block";
 }
-}*/
+}
 
-function displayMessage() {
+/*function displayMessage() {
     let theDate = new Date();
     if(theDate.getDay()==5){
         document.querySelector(".message").style.display = "block";
     }
-}
+}*/
