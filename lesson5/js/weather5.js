34e1d005cf6ebc3268d545bcad56c78bfa1b896a
virginia517 <virginiavirginia718@gmail.com>
2020-10-16 11:39:44 -0700

    update

--- lesson5/js/weather5.js
+++ lesson5/js/weather5.js
@@ -3,6 +3,3 @@ let dayOfWeek= now.getDay();
 if(dayOfWeek==5){
 	document.getElementsByClassName("bannerAd").style.display="block";
 }
-else{
-	document.getElementsByClassName("bannerAd").style.display="none";
-}
