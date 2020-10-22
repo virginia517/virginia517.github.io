ccf4d66e88f872d35c8fe821206ba7185c6525c2
virginia517 <virginiavirginia718@gmail.com>
2020-10-21 17:44:46 -0700

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,8 +1,8 @@
 displaymessage();
 function displaymessage(){
-    let t= 51;
+    let t= 54;
     let w= 12;
     let compute= 35.74+0.6215*t-35.75*Math.pow(w,0.16)+0.4275*t*Math.pow(w,0.16);
 
-    document.querySelector(".windChill").innerHTML= compute;
+    document.querySelector(".windChill").textContent= compute.toFixed(2) +" "+"°F";
 }
