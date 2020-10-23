d1b04f88b15d452429dcea7dca8265375ef5ce9d
virginia517 <virginiavirginia718@gmail.com>
2020-10-23 18:01:05 +0800

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,17 +1,15 @@
 //displaymessage();
-windChill();
-function windChill(){
+
+
     let t= parseFloat(document.getElementById('temp').textContent);
     let w= parseFloat(document.getElementById('windSpeed').textContent);
-    let output="";
+    let compute= 35.74+0.6215*t-35.75*Math.pow(w,0.16)+0.4275*t*Math.pow(w,0.16);
     if(t <= 50 && w >= 3){
-        let compute= 35.74+0.6215*t-35.75*Math.pow(w,0.16)+0.4275*t*Math.pow(w,0.16);
-        output=Math.round(compute);
+        document.querySelector(".windChill").textContent= Math.ceil(compute);
     }
     else{
-        output="N/A";
+        document.querySelector(".windChill").textContent=  "N/A";
     }
-    
-document.querySelector(".windChill").textContent= output;
+
     //document.querySelector(".windChill").textContent= output.toFixed()+" "+"°F";
-}
+
