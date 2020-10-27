7a688100c054f74181e9de4bd686329a136fa899
virginia517 <virginiavirginia718@gmail.com>
2020-10-27 20:40:32 +0800

    update

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,17 +1,17 @@
-let imagesToLoad = document.querySelectorAll("img[data-src]");
+let imagesToLoad = document.querySelectorAll("img[data-src]");//contains the reference to all the images
 const imgOption = {
     threshold:1,
     rootMargin: "0px 0px 50px 0px"
 
 };
-const loadImages = (image) => {
+const loadImages = (image) => {//moves the image path from data-src to src
     image.setAttribute("src", image.getAttribute("data-src"));
-    image.onload = () => {
-        image.removeAttribute("data-src");
+    image.onload = () => {//loop through image and load it
+        image.removeAttribute("data-src");//remove the data-src attribute because it is not needed anymore
     };
 };
 
-if('IntersectionObserver' in window) {
+if('IntersectionObserver' in window) {//intersection observer causes the image to load in the vieport only when the user scrolls down
     const observer = new IntersectionObserver((items, observer) => {
       items.forEach((item) => {
         if(item.isIntersecting) {
