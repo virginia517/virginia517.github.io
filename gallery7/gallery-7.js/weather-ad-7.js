10c807bca477b7ec88aa5f6982f56070db5fbe69
virginia517 <virginiavirginia718@gmail.com>
2020-10-27 11:05:33 +0800

    update

---
+++ gallery7/gallery-7.js/weather-ad-7.js
@@ -0,0 +1,17 @@
+
+displaymessage();
+function displaymessage() {
+	let now= new Date();
+    let dayOfWeek= now.getDay();
+    if(dayOfWeek==5){
+	document.querySelector(".message").style.display="block";
+}
+}
+
+/*displaymessage();
+function displaymessage(){
+    let theDate = new Date();
+    if (theDate.getDay()==5){
+        document.querySelector(".message").style.display = "block";
+    }
+}*/
