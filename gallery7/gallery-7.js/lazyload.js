4137ee620b65bd1d2a5258e5f13a95139a19bfb4
virginia517 <virginiavirginia718@gmail.com>
2020-10-29 20:41:35 +0800

    update

---
+++ gallery7/gallery-7.js/lazyload.js
@@ -0,0 +1,31 @@
+let imagesToLoad = document.querySelectorAll("img[data-src]");//contains the reference to all the images
+const imgOption = {
+    threshold:1,
+    rootMargin: "0px 0px 50px 0px"
+
+};
+const loadImages = (image) => {//moves the image path from data-src to src
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {//loop through image and load it
+        image.removeAttribute("data-src");//remove the data-src attribute because it is not needed anymore
+    };
+};
+
+//this if statement checks if the intersection observer is supported
+if('IntersectionObserver' in window) {//intersection observer causes the image to load in the vieport only when the user scrolls down
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if(item.isIntersecting) {//if image is in the window, load the image
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
+    });
+    imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {//if not supported, load anyway
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
