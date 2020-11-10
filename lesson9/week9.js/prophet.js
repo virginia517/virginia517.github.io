0ae850d5ed9273278c77884cc281572457871699
virginia517 <virginiavirginia718@gmail.com>
2020-11-10 11:33:06 +0800

    update

--- lesson9/week9.js/prophet.js
+++ lesson9/week9.js/prophet.js
@@ -1,1 +1,8 @@
 const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+fetch(requestURL)
+.then(function (response){
+    return response.json();
+})
+.then(function (jsonObject){
+    console.table(jsonObject);
+});
