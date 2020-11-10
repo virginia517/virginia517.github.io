cf15bfb16acdce36145ce3950ed4e456cf56149c
virginia517 <virginiavirginia718@gmail.com>
2020-11-10 12:01:40 +0800

    update

--- lesson9/week9.js/prophet.js
+++ lesson9/week9.js/prophet.js
@@ -5,4 +5,15 @@ fetch(requestURL)
 })
 .then(function (jsonObject){
     console.table(jsonObject);
+    for(let i=0; i<prophets.length; i++){
+        let card= document.createElement('secction');
+        let h2= document.createElement('h2');
+        h2.textContent=prophets[i].name + ' ' + prophets[i].lastname;
+        card.appendChild(h2);
+        document.querySelector('div.cards').appendchild(card);
+    }
 });
+
+//store the results of the converted response into an array
+const prophets= jsonObject['prophets'];
+
