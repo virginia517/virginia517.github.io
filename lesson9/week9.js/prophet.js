3d30ecca77785906d77c0e08dfbd23768da44f11
virginia517 <virginiavirginia718@gmail.com>
2020-11-10 12:57:07 +0800

    update

--- lesson9/week9.js/prophet.js
+++ lesson9/week9.js/prophet.js
@@ -11,12 +11,15 @@ fetch(requestURL)
         let h2= document.createElement('h2');
         let h3= document.createElement('h3'); 
         let p= document.createElement('p');
+        let image= document.createElement('image');
         h2.textContent=prophets[i].name + ' ' + prophets[i].lastname;
         card.appendChild(h2);
         h3.textContent='Date of Birth:' + prophets[i].birthdate;
         card.appendChild(h3);
         p.textContent='Place of Birth:' + prophets[i].birthplace;
         card.appendChild(p);
+        image.setAttribute('src', prophets[i].imageurl);
+        card.appendChild(image);
         document.querySelector('div.cards').appendChild(card);
        }
      
