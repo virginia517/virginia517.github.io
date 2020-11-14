d43fac2a80c14aa522662315de25a26b03f8ab70
virginia517 <virginiavirginia718@gmail.com>
2020-11-14 18:33:58 +0800

    update

--- lesson9/week9.js/homepage.js
+++ lesson9/week9.js/homepage.js
@@ -1,8 +1,9 @@
 const requestUrl= "https://byui-cit230.github.io/weather/data/towndata.json";
 fetch(requestURL)
-.then(function (response){
+  .then(function (response) {
     return response.json();
-})
-.then(function (jsonObject){
-    console.table(jsonObject);
-});
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const towns= jsonObject['towns'];
+  });
