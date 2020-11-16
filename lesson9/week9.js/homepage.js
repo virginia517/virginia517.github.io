2d4b66951812e2aa6c6305229908681f4d547dac
virginia517 <virginiavirginia718@gmail.com>
2020-11-16 15:34:04 +0800

    update

--- lesson9/week9.js/homepage.js
+++ lesson9/week9.js/homepage.js
@@ -14,12 +14,37 @@ fetch(requestUrl)
             let p2= document.createElement('p');
             let p3= document.createElement('p');
             let p4= document.createElement('p');
-            let image= document.createElement('p');
+            let image= document.createElement('img');
             card.appendChild(h1);
             card.appendChild(p1);
             card.appendChild(p2);
             card.appendChild(p3);
             card.appendChild(p4);
+            card.appendChild(image);
+            card.appendChild(towninfo);
+
+            //card.setAttribute('class', 'townsect');
+
+           // towninfo.setAttribute('class', 'towninfo');
+
+            h1.textContent = towns[i].name;
+            h1.appendChild(h1);
+
+            motto.textContent = towns[i].motto;
+            towninfo.appendChild(motto);
+
+            founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
+            towninfo.appendChild(founded);
+
+            population.textContent = 'Population: ' + towns[i].currentPopulation;
+            towninfo.appendChild(population);
+
+            rainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+            towninfo.appendChild(rainfall);
+            
+            image.setAttribute('class', 'townimg');
+            image.setAttribute('src', 'assets/' + towns[i].photo);
+            image.setAttribute('alt', 'Image of ' + towns[i].name);
             card.appendChild(image);
             document.querySelector('div.home').appendChild(card);
         }
