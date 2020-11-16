e023fff3ece83bb70a1ce29d9b91831029373ec2
virginia517 <virginiavirginia718@gmail.com>
2020-11-16 14:10:19 +0800

    update

--- lesson9/week9.js/homepage.js
+++ lesson9/week9.js/homepage.js
@@ -6,6 +6,15 @@ fetch(requestUrl)
 .then(function (jsonObject){
     console.table(jsonObject);
     const towns= jsonObject['towns'];
+    for(let i=0; i<towns.length; i++){
+        let card= document.createElement('section');
+        let p= document.createElement('p');
+        let p1= document.createElement('p');
+        let p2= document.createElement('p');
+        let p3= document.createElement('p');
+        h1.textContent=towns[i].name;
+        card.appendChild(h1);
+    }
 });
 
 
