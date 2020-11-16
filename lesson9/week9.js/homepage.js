27ffda6a477dbfd8d0bc55bef02946c28d68d47a
virginia517 <virginiavirginia718@gmail.com>
2020-11-16 16:07:22 +0800

    update

--- lesson9/week9.js/homepage.js
+++ lesson9/week9.js/homepage.js
@@ -7,45 +7,42 @@ fetch(requestUrl)
     console.table(jsonObject);
     const towns= jsonObject['towns'];
     for(let i=0; i<towns.length; i++){
-        if(towns[i].name=="Preston" || towns[i].name=="Fish Haven" || towns[i].name=="Soda Spring"){
-            let card= document.createElement('section');
-            let h1= document.createElement('h1');
-            let p1= document.createElement('p');
-            let p2= document.createElement('p');
-            let p3= document.createElement('p');
-            let p4= document.createElement('p');
-            let image= document.createElement('img');
-            card.appendChild(h1);
-            card.appendChild(p1);
-            card.appendChild(p2);
-            card.appendChild(p3);
-            card.appendChild(p4);
-            card.appendChild(image);
-            card.appendChild(towninfo);
+        if(towns[i].name=="Preston" || towns[i].name=="Fish Haven" || towns[i].name=="Soda Springs"){
+            let card = document.createElement('section');
+            let info = document.createElement('section');
+            let h2 = document.createElement('h2');
+            let motto = document.createElement('p');
+            let founded = document.createElement('p');
+            let population = document.createElement('p');
+            let rainfall = document.createElement('p');
+            let image = document.createElement('img');
+            
+            card.appendChild(info);
 
-            //card.setAttribute('class', 'townsect');
+            card.setAttribute('class', 'townsect');
 
-           // towninfo.setAttribute('class', 'towninfo');
+            info.setAttribute('class', 'info');
 
-            h1.textContent = towns[i].name;
-            h1.appendChild(h1);
+            h2.textContent = towns[i].name;
+            info.appendChild(h2);
 
             motto.textContent = towns[i].motto;
-            towninfo.appendChild(motto);
+            info.appendChild(motto);
 
             founded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            towninfo.appendChild(founded);
+            info.appendChild(founded);
 
             population.textContent = 'Population: ' + towns[i].currentPopulation;
-            towninfo.appendChild(population);
+            info.appendChild(population);
 
             rainfall.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
-            towninfo.appendChild(rainfall);
+            info.appendChild(rainfall);
             
             image.setAttribute('class', 'townimg');
-            image.setAttribute('src', 'assets/' + towns[i].photo);
+            image.setAttribute('src', 'images/' + towns[i].photo);
             image.setAttribute('alt', 'Image of ' + towns[i].name);
             card.appendChild(image);
+          
             document.querySelector('div.home').appendChild(card);
         }
     }
