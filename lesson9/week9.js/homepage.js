1b75ffc9e1968ceb7fa68f240293249e844523fa
virginia517 <virginiavirginia718@gmail.com>
2020-11-16 15:29:14 +0800

    update

--- lesson9/week9.js/homepage.js
+++ lesson9/week9.js/homepage.js
@@ -9,6 +9,18 @@ fetch(requestUrl)
     for(let i=0; i<towns.length; i++){
         if(towns[i].name=="Preston" || towns[i].name=="Fish Haven" || towns[i].name=="Soda Spring"){
             let card= document.createElement('section');
+            let h1= document.createElement('h1');
+            let p1= document.createElement('p');
+            let p2= document.createElement('p');
+            let p3= document.createElement('p');
+            let p4= document.createElement('p');
+            let image= document.createElement('p');
+            card.appendChild(h1);
+            card.appendChild(p1);
+            card.appendChild(p2);
+            card.appendChild(p3);
+            card.appendChild(p4);
+            card.appendChild(image);
             document.querySelector('div.home').appendChild(card);
         }
     }
