48b8bbe745154ac9d55da080496651e407405754
virginia517 <virginiavirginia718@gmail.com>
2020-11-19 13:13:08 +0800

    update

--- lesson10/week10.js/preston10.js
+++ lesson10/week10.js/preston10.js
@@ -1,9 +1,11 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=dcc0b52259c23e2902cb56cc6c6157f9";
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=dcc0b52259c23e2902cb56cc6c6157f9";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('temp').textContent = jsObject.main.temp;
-
+    document.getElementById('condition').textContent = jsObject.weather[0].description;
+    document.getElementById('temp').textContent=jsObject.main.temp;
+    document.getElementById('humid').textContent=jsObject.main.humidity;
+    document.getElementById('windSpeed').textContent=jsObject.wind.speed;
 
   });
