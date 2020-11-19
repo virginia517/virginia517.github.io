a0b65865f035f9fbfba6c690654de27a26198c72
virginia517 <virginiavirginia718@gmail.com>
2020-11-19 10:55:42 +0800

    update

---
+++ lesson10/week10.js/preston10.js
@@ -0,0 +1,7 @@
+const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=dcc0b52259c23e2902cb56cc6c6157f9";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    
+  });
