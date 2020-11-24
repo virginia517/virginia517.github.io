504ddf60523df3760a0c1cf5026ca8796d7774a6
virginia517 <virginiavirginia718@gmail.com>
2020-11-24 14:58:25 +0800

    update

---
+++ lesson11/js/sodaspringswindchill.js
@@ -0,0 +1,15 @@
+//displaymessage();
+
+
+    let t= parseFloat(document.getElementById('temp').textContent);
+    let w= parseFloat(document.getElementById('windSpeed').textContent);
+    let compute= 35.74+0.6215*t-35.75*Math.pow(w,0.16)+0.4275*t*Math.pow(w,0.16);
+    if(t <= 50 && w >= 3){
+        document.querySelector(".windChill").textContent= Math.ceil(compute)+ "°F";
+    }
+    else{
+        document.querySelector(".windChill").textContent=  "N/A";
+    }
+
+    //document.querySelector(".windChill").textContent= output.toFixed()+" "+"°F";
+
