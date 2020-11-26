52d84f9567bffc1c14edca2691c61513b57f6ca9
virginia517 <virginiavirginia718@gmail.com>
2020-11-26 11:52:44 +0800

    update

--- lesson11/js/sodasprings.js
+++ lesson11/js/sodasprings.js
@@ -1,5 +1,5 @@
-const apiUrl = "http://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=dcc0b52259c23e2902cb56cc6c6157f9";
-const apUrl = "http://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=dcc0b52259c23e2902cb56cc6c6157f9";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?id=5607916&units=imperial&appid=dcc0b52259c23e2902cb56cc6c6157f9";
+const apUrl = "https://api.openweathermap.org/data/2.5/forecast?id=5607916&units=imperial&appid=dcc0b52259c23e2902cb56cc6c6157f9";
 fetch(apiUrl)
   .then((response) => response.json())
   .then((jsObject) => {
