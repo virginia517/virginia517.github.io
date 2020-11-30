476c5ebefcf106373c99da39e7c71056fabee7b0
virginia517 <virginiavirginia718@gmail.com>
2020-12-01 06:37:36 +0800

    update

--- finalProject/js/index.js
+++ finalProject/js/index.js
@@ -1,3 +1,33 @@
 function toggleMenu() {
     document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = dayName + ", " + monthName + " " + d.getDate() +", " + year;
+document.getElementById("currentDate").textContent = fulldate;
+
