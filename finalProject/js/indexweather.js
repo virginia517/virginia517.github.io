828718149d59470538b79f0c572f2d78ffe4d4cf
virginia517 <virginiavirginia718@gmail.com>
2020-12-01 17:24:09 +0800

    update

--- finalProject/js/indexweather.js
+++ finalProject/js/indexweather.js
@@ -1,1 +1,6 @@
-const url="api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}"
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&appid=dcc0b52259c23e2902cb56cc6c6157f9";
+fetch(apiUrl)
+  .then((response) => response.json())
+  .then((jsObject) => {
+   console.log(jsObject);
+});
