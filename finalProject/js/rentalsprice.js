6b8c5ab70b28879a471780fb35717b1ec4e55a27
virginia517 <virginiavirginia718@gmail.com>
2020-12-02 17:44:50 +0800

    update

--- finalProject/js/rentalsprice.js
+++ finalProject/js/rentalsprice.js
@@ -69,4 +69,6 @@ let rentals= `[{
 console.log(JSON.parse(rentals));
 console.log(JSON.parse(rentals)[5].name);
 
-document.getElementById('hondares1').textContent = JSON.parse(rentals)[0].name.fullday;
+document.getElementById('hondares2').textContent = JSON.parse(rentals)[0].reservation.fullday;
+document.getElementById('hondares1').textContent = JSON.parse(rentals)[0].reservation.halfday;
+document.getElementById('hondawalk1').textContent = JSON.parse(rentals)[0].walkin.halfday;
